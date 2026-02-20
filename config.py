96f889fae2eca717cdbb62895e46bbf0a27b48d2
SegflowJP <cicadafourteen@gmail.com>
2026-02-20 22:06:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     # Line Messaging API Settings
     # These should be obtained from the Line Developers Console
-    LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('RpTjCFT+6LnqwwDanjRFJjf+8+UUDRWGcxTVRWZSMdGIp6P3e1RIkL4iIQsF6rb1AIA42aaeZu60Q9MAgYGrgOJ1hN7aoMGsYN68jwKXFmpDIlwMHsoHPEhBRNx9PMUQ6toQAC+lBjO0godl1SwrYQdB04t89/1O/w1cDnyilFU=', 'RpTjCFT+6LnqwwDanjRFJjf+8+UUDRWGcxTVRWZSMdGIp6P3e1RIkL4iIQsF6rb1AIA42aaeZu60Q9MAgYGrgOJ1hN7aoMGsYN68jwKXFmpDIlwMHsoHPEhBRNx9PMUQ6toQAC+lBjO0godl1SwrYQdB04t89/1O/w1cDnyilFU=')
-    LINE_CHANNEL_SECRET = os.environ.get('2610ad3b734526f5d9dfac540c4aba44', '2610ad3b734526f5d9dfac540c4aba44')
+    LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN', 'YOUR_CHANNEL_ACCESS_TOKEN')
+    LINE_CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET', 'YOUR_CHANNEL_SECRET')
     
     # Manager's Line User ID to receive reports
-    MANAGER_USER_ID = os.environ.get('U1c537b1aaabb943df75b40cd148eab2e', 'U1c537b1aaabb943df75b40cd148eab2e')
+    MANAGER_USER_ID = os.environ.get('MANAGER_USER_ID', 'YOUR_MANAGER_USER_ID')
     
     # Wake-up Check Settings
     CHECK_IN_START_TIME = "06:00" # 6:00 AM
