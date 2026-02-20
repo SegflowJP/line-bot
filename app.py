45c67363215a1efae70b83d6d6342745acf9de49
SegflowJP <cicadafourteen@gmail.com>
2026-02-20 11:49:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import json
 app = Flask(__name__)
 
 CHANNEL_SECRET = os.getenv("2610ad3b734526f5d9dfac540c4aba44")
-CHANNEL_ACCESS_TOKEN = os.getenv("RpTjCFT+6LnqwwDanjRFJjf+8+UUDRWGcxTVRWZSMdGIp6P3e1RIkL4iIQsF6rb1AIA42aaeZu60Q9MAgYGrgOJ1hN7aoMGsYN68jwKXFmpDIlwMHsoHPEhBRNx9PMUQ6toQAC+lBjO0godl1SwrYQdB04t89/1O/w1cDnyilFU=', 'RpTjCFT+6LnqwwDanjRFJjf+8+UUDRWGcxTVRWZSMdGIp6P3e1RIkL4iIQsF6rb1AIA42aaeZu60Q9MAgYGrgOJ1hN7aoMGsYN68jwKXFmpDIlwMHsoHPEhBRNx9PMUQ6toQAC+lBjO0godl1SwrYQdB04t89/1O/w1cDnyilFU=")
+CHANNEL_ACCESS_TOKEN = os.getenv("RpTjCFT+6LnqwwDanjRFJjf+8+UUDRWGcxTVRWZSMdGIp6P3e1RIkL4iIQsF6rb1AIA42aaeZu60Q9MAgYGrgOJ1hN7aoMGsYN68jwKXFmpDIlwMHsoHPEhBRNx9PMUQ6toQAC+lBjO0godl1SwrYQdB04t89/1O/w1cDnyilFU=")
 
 @app.route("/")
 def home():
@@ -20,9 +20,12 @@ def webhook():
     body = request.get_data(as_text=True)
     signature = request.headers.get("X-Line-Signature")
 
+    if CHANNEL_SECRET is None:
+        return "Missing Channel Secret", 500
+
     hash = hmac.new(
-        CHANNEL_SECRET.encode('utf-8'),
-        body.encode('utf-8'),
+        CHANNEL_SECRET.encode("utf-8"),
+        body.encode("utf-8"),
         hashlib.sha256
     ).digest()
 
@@ -31,10 +34,10 @@ def webhook():
     if signature != expected_signature:
         abort(400)
 
-    events = json.loads(body)["events"]
+    events = json.loads(body).get("events", [])
 
     for event in events:
-        if event["type"] == "message":
+        if event["type"] == "message" and event["message"]["type"] == "text":
             reply_token = event["replyToken"]
             user_message = event["message"]["text"]
             reply_message(reply_token, f"You said: {user_message}")
